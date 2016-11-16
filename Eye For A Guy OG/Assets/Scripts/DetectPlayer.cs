20b47250b46c232e002b87925986908692920b5a
Mikey Hall <hallmi20@msu.edu>
2016-11-16 11:02:32 -0500

    enemy chases player
    SetDestination(player.position)

---
+++ Eye For A Guy OG/Assets/Scripts/DetectPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectPlayer : MonoBehaviour
+{
+    [SerializeField]
+    public Transform player;
+    public float minDistance = 10f;
+    public float fovAngle = 30f;
+
+    public float range;
+    public RaycastHit hitInfo;
+    public float angle;
+
+
+    public void FixedUpdate() {
+        range = Vector3.Distance(transform.position, player.position);
+
+        Vector3 targetDir = player.position - transform.position;
+        angle = Vector3.Angle(targetDir, transform.forward);
+
+        if (CanPlayerBeSeen())
+        {
+            Debug.Log("Player Visible");
+        }
+        else
+        { 
+            Debug.Log("Player Not Seen");
+        }
+    }
+
+    public bool CanPlayerBeSeen() {
+        if (range < minDistance && !Physics.Linecast(transform.position, player.position)) {
+            if (PlayerInBeam()) {
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public bool PlayerInBeam() {
+        if (angle < fovAngle) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
