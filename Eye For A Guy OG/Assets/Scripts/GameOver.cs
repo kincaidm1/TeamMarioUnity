c32b78b2434c05529763ecc87ef555faccf5ca38
Unlucky33 <maxdudley33@gmail.com>
2016-12-03 21:10:20 -0500

    Added Start Screen and Game Over

---
+++ Eye For A Guy OG/Assets/Scripts/GameOver.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+    public Canvas gameOver;
+    public Button retryBtn;
+    public Button quitBtn;
+
+	void Awake ()
+    {
+        gameOver = GetComponent<Canvas>();
+        retryBtn = GetComponent<Button>();
+        quitBtn = GetComponent<Button>();
+        gameOver.enabled = false;
+    }
+
+    public void playerCaught ()
+    {
+        gameOver.enabled = true;
+        Time.timeScale = 0;
+    }
+
+    public void Retry ()
+    {
+        gameOver.enabled = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit ()
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+}
---
+++ Eye For A Guy OG/Assets/Scripts/StartScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartScript : MonoBehaviour
+{
+
+    public Canvas mainMenu;
+    public Button startBtn;
+    public Button exitBtn;
+
+    void Awake()
+    {
+        mainMenu = GetComponent<Canvas>();
+        startBtn = GetComponent<Button>();
+        exitBtn = GetComponent<Button>();
+    }
+
+    public void ClickStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+
+}
